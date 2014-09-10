92f4d5d7aaad565a6dd95f0dbd96fccd2dd19136
snehab14 <sbharadw@usc.edu>
2014-09-09 22:26:39 -0700

    added tcp client to the udp server

--- udp_lossles_test/udp_server.c
+++ udp_lossles_test/udp_server.c
@@ -15,13 +15,92 @@
 
 #include "customhead.h"
 
+//tcp client
+#include "log.h"
+#include <pthread.h>
+
+pthread_t tcp_client;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+struct mytcpheader head;
+
+
+// thread for tcp client
+
+void *arq_send(void *args){
+
+    int sockfd, n, i, count;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    int arr[MAX_ARQ_SIZE/16] = {0};
+
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        LOGERR("ERROR opening socket");
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        LOGERR("ERROR, no such host\n");
+        exit(0); 
+        }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(TCP_PORT);
+
+     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        LOGERR("ERROR connecting");
+
+while(1){
+    pthread_mutex_lock(&lock);
+
+    arr = head.nack_array;
+ 
+    pthread_mutex_unlock(&lock);
+
+    count = 0;
+    for(i =0, i<sizeof(arr), i++){
+        if (arr[i] == 0)
+            count++;
+    }
+
+    if (0 == count)
+        pthread_exit(0);
+
+    usleep(count* PACKET_TIME);
+
+    pthread_mutex_lock(&lock);
+
+    arr = head.nack_array;
+ 
+    pthread_mutex_unlock(&lock);
+
+
+    n = send(sd, (void*)&arr, sizeof(arr), 0);
+        if (n < 0) 
+            LOGERR("ERROR writing to socket");
+    
+}
+    close(sockfd);
+
+}
+
 int main()
 {
-        int sock;
-        socklen_t addr_len;
-	int  bytes_read;
-        char recv_data[sizeof(struct myudpheader)];
-        struct sockaddr_in server_addr , client_addr;
+
+    int sock;
+    socklen_t addr_len;
+    int  bytes_read;
+    char recv_data[sizeof(struct myudpheader)];
+    struct sockaddr_in server_addr , client_addr;
+
+
+    if(pthread_create(&tcp_client, NULL, arq_send, "tcp_client") {
+        LOGERR("Error creating thread\n");  
+        return 1;
+        }
+
 
 
         if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
@@ -62,6 +141,10 @@ int main()
 //	  fflush(stdout);          
 //printf("\n data =");
 	memcpy(heap_mem+(recv_payload->sequence_no)*MAX_DATA_SIZE,recv_payload->payload_data,MAX_DATA_SIZE);
+
+
+    head.nack_array[recv_payload->sequence_no] = 1;
+    
 	if(recv_payload->sequence_no == MAX_DATA_SIZE-1)
 	 {
 	  printf("\n Writing to file\n");
