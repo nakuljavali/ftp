995c4fa27f0a428402b01cbed92fc7d9bc66ea11
Nakul Nagareshwar Javali <sc558bb@users.isi.deterlab.net>
2014-09-09 22:16:43 -0700

    adder tcp server to the client code

--- udp_lossles_test/macros.h
+++ udp_lossles_test/macros.h
@@ -1,6 +1,6 @@
 
 #define MAX_DATA_SIZE 32768
-#define MAX_ARQ_SIZE 256
+#define MAX_ARQ_SIZE 32768
 #define MAX_BATCH_SIZE 128
 
 #define SERVER_IP "10.1.1.3"
@@ -8,3 +8,5 @@
 
 #define UDP_PORT 7654
 #define TCP_PORT 4567
+
+#define PACKET_TIME 100
