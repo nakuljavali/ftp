c3046179d879c695c18877eeb2ca0896f5fec6a1
Niranjan Bandakkala Shivakumarappa <sc558ae@users.isi.deterlab.net>
2014-09-13 17:18:22 -0700

    updated myparameters.c

--- myparameters.c
+++ myparameters.c
@@ -26,34 +26,163 @@ int last_batch_size = 0;
 int last_packet_size = 0;
 */
 
+
 void  fill_parameters(char *input_file,int pkt_size,int bch_size)
+
 {
+
+
+
 	int file=0;
+
      	if((file=open(input_file,O_RDONLY)) < -1)
+
             exit(1);
+
  
+
 	struct stat fileStat;
+
     	if(fstat(file,&fileStat) < 0)    
+
             exit(1);
+
+
+
 
 
 	filesize = fileStat.st_size;
+
 	packet_size = pkt_size;
+
 	batch_size = bch_size;
-        no_of_packets = filesize / packet_size;
-	no_of_batches = no_of_packets / batch_size;
-	last_batch_size = no_of_packets -  no_of_batches * batch_size;
-        if(no_of_packets % batch_size == 0) last_batch_size = batch_size;
-	last_packet_size = filesize - no_of_packets * packet_size;
-        if(filesize % packet_size == 0) last_packet_size = packet_size;
+
+
+
+
+
+	if(filesize < packet_size){
+
+		no_of_batches = 1;
+
+		no_of_packets = 1;
+
+		last_batch_size = 1;
+
+		last_packet_size = filesize;
+
+		batch_size = 1;
+
+	}
+
+
+
+	else {
+
+		no_of_packets = filesize / packet_size;
+
+		if(filesize%packet_size !=0)
+
+			no_of_packets++;
+
+		printf("batch size: %d\n", batch_size);
+
+		printf("no of packets: %d\n", no_of_packets);
+
+
+
+		if (no_of_packets <= batch_size) {
+
+			batch_size = no_of_packets;
+
+			no_of_batches = 1;
+
+			last_batch_size = batch_size;
+
+			if(filesize%packet_size != 0)
+
+				last_packet_size = filesize%packet_size;
+
+			else
+
+				last_packet_size = packet_size;
+
+			printf("here\n");
+
+
+
+		}
+
+
+
+
+
+		else {
+
+			no_of_batches = no_of_packets / batch_size;
+
+
+
+			if(no_of_packets%batch_size != 0)
+
+				no_of_batches++;
+
+
+
+	        if(no_of_packets % batch_size == 0){
+
+
+
+
+
+	        	last_batch_size = batch_size;
+
+	        }
+
+	        else{
+
+	        	last_batch_size = no_of_packets % batch_size;
+
+
+
+	        }
+
+
+
+	        if(filesize % packet_size == 0)
+
+	        	last_packet_size = packet_size;
+
+	        else
+
+	        	last_packet_size = filesize%packet_size;
+
+
+
+		}
+
+}
+
+
+
 	printf("Information for %s\n",input_file);
+
    	printf("---------------------------\n");
+
     	printf("File Size:         %ld bytes\n",fileStat.st_size);
+
     	printf("Packet Size:       %d bytes\n",packet_size);
+
     	printf("No of Packets:     %d bytes\n",no_of_packets);
+
     	printf("Batch size:        %d bytes\n",batch_size);
+
     	printf("No of batches:     %d bytes\n",no_of_batches);
+
     	printf("Last Packet Size:  %d bytes\n",last_packet_size);
+
     	printf("Last Batch Size:   %d bytes\n",last_batch_size);
+
 	
+
 }
